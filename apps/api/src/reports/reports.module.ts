84a6ced53071e7ed2a1772e28fb51ca561b2155b
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-05-23 11:18:53 -0300

    feat(reports): registra services da Reports API

--- apps/api/src/reports/reports.module.ts
+++ apps/api/src/reports/reports.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { SqlServerModule } from '../sql-server/sql-server.module';
 import { ReportDefinitionsRepository } from './repositories/report-definitions.repository';
+import { ReportAuthorizationService } from './report-authorization.service';
 import { ReportDefinitionsAdminController } from './report-definitions.admin.controller';
 import { ReportDefinitionsService } from './report-definitions.service';
+import { ReportsApiService } from './reports-api.service';
 import { ReportsController } from './reports.controller';
 
 @Module({
+  imports: [SqlServerModule],
   controllers: [ReportDefinitionsAdminController, ReportsController],
-  providers: [ReportDefinitionsRepository, ReportDefinitionsService],
-  exports: [ReportDefinitionsRepository, ReportDefinitionsService],
+  providers: [ReportDefinitionsRepository, ReportDefinitionsService, ReportAuthorizationService, ReportsApiService],
+  exports: [ReportDefinitionsRepository, ReportDefinitionsService, ReportAuthorizationService, ReportsApiService],
 })
 export class ReportsModule {}
